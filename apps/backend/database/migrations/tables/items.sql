794288bd956cd50789d16cf1c0193dd6d73949f5
FerdiHS <ferdi.fhs.fhs@gmail.com>
2026-01-28 02:24:09 +0800

    Create the items table

---
+++ apps/backend/database/migrations/tables/items.sql
@@ -0,0 +1,20 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TYPE item_status AS ENUM ('draft','active','ended','cancelled');
+
+CREATE TABLE IF NOT EXISTS items (
+	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	seller_id uuid NOT NULL REFERENCES users(id),
+	time_start timestamptz NOT NULL,
+	time_end timestamptz NOT NULL,
+	title text NOT NULL,
+	description text,
+	author text,
+	features jsonb,
+	base_price bigint NOT NULL CHECK (base_price >= 0),
+	increment bigint NOT NULL CHECK (increment > 0),
+	status item_status NOT NULL DEFAULT 'draft',
+	created_at timestamptz NOT NULL DEFAULT now(),
+	updated_at timestamptz NOT NULL DEFAULT now(),
+	CHECK (time_end > time_start)
+);

4082931a1e8ed2712024dd8046602bd592925e17
FerdiHS <ferdi.fhs.fhs@gmail.com>
2026-01-28 02:24:36 +0800

    Create the bids table

---
+++ apps/backend/database/migrations/tables/bids.sql
@@ -0,0 +1,9 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE IF NOT EXISTS bids (
+	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	user_id uuid NOT NULL REFERENCES users(id),
+	item_id uuid NOT NULL REFERENCES items(id) ON DELETE CASCADE,
+	price bigint NOT NULL CHECK (price > 0),
+	timestamp timestamptz NOT NULL DEFAULT now()
+);

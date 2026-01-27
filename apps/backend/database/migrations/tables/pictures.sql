bbeee9ecbfef44dae8b897598339a40ad2401e32
FerdiHS <ferdi.fhs.fhs@gmail.com>
2026-01-28 02:24:24 +0800

    Create the pictures table

---
+++ apps/backend/database/migrations/tables/pictures.sql
@@ -0,0 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE TABLE IF NOT EXISTS pictures (
+	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+	item_id uuid NOT NULL REFERENCES items(id) ON DELETE CASCADE,
+	url text NOT NULL,
+	created_at timestamptz NOT NULL DEFAULT now()
+);

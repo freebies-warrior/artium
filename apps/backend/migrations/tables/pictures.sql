6741b1ee475363aa1a91e8be730ec825f7829a2b
FerdiHS <ferdi.fhs.fhs@gmail.com>
2026-01-29 21:28:07 +0800

    Update pictures table sql

--- apps/backend/migrations/tables/pictures.sql
+++ apps/backend/migrations/tables/pictures.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS pictures (
 	id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
 	item_id uuid NOT NULL REFERENCES items(id) ON DELETE CASCADE,
-	url text NOT NULL,
+	key text NOT NULL,
 	created_at timestamptz NOT NULL DEFAULT now()
 );
 

5101185d033ed41235327b2370b1476f8b82b271
Andrew Daniel Janong <andrewjanong@gmail.com>
2026-01-29 22:29:18 +0800

    Feat: add endpoint to get upload link

--- apps/backend/handlers/handlers.go
+++ apps/backend/handlers/handlers.go
@@ -1,10 +1,12 @@
 package handlers
 
 import "backend/database"
+import "github.com/aws/aws-sdk-go-v2/service/s3"
 
 type HandlerSet struct {
 	Auth *AuthHandler
 	Items *ItemsHandler
+	Uploads *UploadHandler
 	Bids *BidsHandler
 }
 
@@ -16,10 +18,13 @@ func NewHandlerSet(
 	bids *database.BidDatabase,
 	jwtSecret []byte,
 	appBaseURL string,
+	r2Bucket string,
+	s3Client *s3.Client,
 ) *HandlerSet {
 	return &HandlerSet{
 		Auth: NewAuthHandler(users, tokens, jwtSecret, appBaseURL),
 		Items: NewItemsHandler(items, pictures),
+		Uploads: NewUploadHandler(r2Bucket, s3Client),
 		Bids: NewBidsHandler(bids, items),
 	}
 }
---
+++ apps/backend/handlers/uploads.go
@@ -0,0 +1,109 @@
+package handlers
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"net/http"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"backend/middlewares"
+	"backend/utils"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/gin-gonic/gin"
+)
+
+type UploadHandler struct {
+	bucket    string
+	presigner *s3.PresignClient
+}
+
+type presignReq struct {
+	Filename    string `json:"filename"`
+	ContentType string `json:"content_type"`
+}
+
+type presignPutResp struct {
+	Key       string `json:"key"`
+	UploadURL string `json:"upload_url"`
+}
+
+type presignGetResp struct {
+	Key     string `json:"key"`
+	ViewURL string `json:"view_url"`
+}
+
+func NewUploadHandler(bucket string, s3Client *s3.Client) *UploadHandler {
+	return &UploadHandler{
+		bucket:    bucket,
+		presigner: s3.NewPresignClient(s3Client),
+	}
+}
+
+func (h *UploadHandler) PresignPut(c *gin.Context) {
+	var req presignReq
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, utils.NewError("VALIDATION_ERROR", "invalid json", nil))
+		return
+	}
+
+	ct := strings.TrimSpace(req.ContentType)
+	if !strings.HasPrefix(ct, "image/") {
+		c.JSON(http.StatusBadRequest, utils.NewError("VALIDATION_ERROR", "only image uploads allowed", map[string]any{"field": "content_type"}))
+		return
+	}
+
+	filename := strings.TrimSpace(req.Filename)
+	if filename == "" {
+		filename = "upload"
+	}
+
+	uid := c.GetString(middlewares.CtxUserIDKey)
+	if uid == "" {
+		uid = "anonymous"
+	}
+
+	ext := strings.ToLower(filepath.Ext(filename))
+	if ext == "" {
+		switch ct {
+		case "image/jpeg":
+			ext = ".jpg"
+		case "image/png":
+			ext = ".png"
+		case "image/webp":
+			ext = ".webp"
+		}
+	}
+
+	key := "uploads/" + uid + "/" + time.Now().UTC().Format("20060102T150405Z") + "-" + randHex(16) + ext
+
+	out, err := h.presigner.PresignPutObject(
+		c.Request.Context(),
+		&s3.PutObjectInput{
+			Bucket:      aws.String(h.bucket),
+			Key:         aws.String(key),
+			ContentType: aws.String(ct),
+		},
+		func(o *s3.PresignOptions) {
+			o.Expires = 10 * time.Minute
+		},
+	)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, utils.NewError("INTERNAL_ERROR", "failed to presign upload url", nil))
+		return
+	}
+
+	c.JSON(http.StatusOK, presignPutResp{
+		Key:       key,
+		UploadURL: out.URL,
+	})
+}
+
+func randHex(nBytes int) string {
+	b := make([]byte, nBytes)
+	_, _ = rand.Read(b)
+	return hex.EncodeToString(b)
+}

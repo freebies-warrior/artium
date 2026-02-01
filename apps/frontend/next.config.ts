57f527a1c5963d5c9fe34b542a1f33cfa2a0f953
Fredy Lawrence <fredylawrence2003@gmail.com>
2026-02-01 15:00:34 +0800

    fix: resolve nextConfig

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -5,8 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '3eb517ed9cc2a0678c1dca2504694bee.r2.cloudflarestorage.com',
-        pathname: '/artium-storage/**',
+        hostname: '*.r2.cloudflarestorage.com',
       },
     ],
   },
